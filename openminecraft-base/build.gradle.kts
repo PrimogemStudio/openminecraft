c9697c30e085b425cc8a1ffcc8127ccc04824fed
Jack253-png <guoxiuchen20170402@163.com>
2024-11-09 07:43:25 +0800

    Add coroutine lib

--- openminecraft-base/build.gradle.kts
+++ openminecraft-base/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api:${properties["openminecraft.log4j2_version"]}")
     implementation("org.apache.logging.log4j:log4j-core:${properties["openminecraft.log4j2_version"]}")
     implementation("org.jetbrains.kotlin:kotlin-reflect:${properties["openminecraft.kotlin_version"]}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${properties["openminecraft.kotlinx_coroutines_core_version"]}")
 }
 
 kotlin {
