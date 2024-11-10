6d3a716824f0465c9142833c04bddc11dfd923ad
Jack253-png <guoxiuchen20170402@163.com>
2024-11-10 18:58:48 +0800

    HarfBuzz setting

--- src/main/kotlin/com/primogemstudio/MainWrapped.kt
+++ src/main/kotlin/com/primogemstudio/MainWrapped.kt
@@ -1,6 +1,7 @@
 package com.primogemstudio
 
 fun main() {
+    System.setProperty("org.lwjgl.harfbuzz.libname", "freetype")
     println(VkInstanceEngine("OpenMinecraft", "0.0.1-alpha1"))
     while (true) {
         Thread.sleep(1000)
