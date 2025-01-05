def66f12fc504198819b37b2aff7f969005334f7
Jack253-png <guoxiuchen20170402@163.com>
2025-01-05 10:48:25 +0800

    Test

--- src/main/kotlin/com/primogemstudio/engine/MainWrapped.kt
+++ src/main/kotlin/com/primogemstudio/engine/MainWrapped.kt
@@ -1,6 +1,8 @@
 package com.primogemstudio.engine
 
+import java.lang.foreign.FunctionDescriptor
 import java.lang.foreign.Linker
+import java.lang.foreign.ValueLayout.OfChar
 
 fun main() {
     /*System.setProperty("org.lwjgl.harfbuzz.libname", "freetype")*/
@@ -8,5 +10,10 @@ fun main() {
     instance.vkWindow!!.mainLoop()*/
 
     val linker = Linker.nativeLinker()
-    println(linker.defaultLookup().find("puts").get().address())
+    println(
+        linker.downcallHandle(
+            linker.defaultLookup().find("sinf").get(),
+            FunctionDescriptor.of(OfChar.JAVA_FLOAT, OfChar.JAVA_FLOAT)
+        ).invoke(0.5f)
+    )
 }
