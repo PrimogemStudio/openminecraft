ff2d8b1036d4188530b5588826cd21feeae0bd19
Jack253-png <guoxiuchen20170402@163.com>
2025-01-13 18:52:16 +0800

    printf 函数调用测试

--- src/main/kotlin/com/primogemstudio/engine/MainWrapped.kt
+++ src/main/kotlin/com/primogemstudio/engine/MainWrapped.kt
@@ -1,19 +1,21 @@
 package com.primogemstudio.engine
 
+import java.lang.foreign.Arena
 import java.lang.foreign.FunctionDescriptor
 import java.lang.foreign.Linker
 import java.lang.foreign.ValueLayout.OfChar
+
 
 fun main() {
     /*System.setProperty("org.lwjgl.harfbuzz.libname", "freetype")*/
     /*val instance = VkInstanceEngine("OpenMinecraft", "0.0.1-alpha1")
     instance.vkWindow!!.mainLoop()*/
 
+    val offHeap = Arena.ofConfined()
+
     val linker = Linker.nativeLinker()
-    println(
-        linker.downcallHandle(
-            linker.defaultLookup().find("sinf").get(),
-            FunctionDescriptor.of(OfChar.JAVA_FLOAT, OfChar.JAVA_FLOAT)
-        ).invoke(0.5f)
-    )
+    linker.downcallHandle(
+        linker.defaultLookup().find("printf").get(),
+        FunctionDescriptor.ofVoid(OfChar.ADDRESS)
+    ).invoke(offHeap.allocateUtf8String("test %llx %llx"))
 }
