fe211aeab96b2c3d7eb32a9227697b18129edd3a
Jack253-png <guoxiuchen20170402@163.com>
2025-01-17 11:40:11 +0800

    GLFW 测试

--- src/main/kotlin/com/primogemstudio/engine/MainWrapped.kt
+++ src/main/kotlin/com/primogemstudio/engine/MainWrapped.kt
@@ -4,8 +4,9 @@ import com.primogemstudio.engine.loader.Platform
 import java.lang.foreign.Arena.ofConfined
 import java.lang.foreign.FunctionDescriptor
 import java.lang.foreign.Linker
-import java.lang.foreign.ValueLayout.ADDRESS
-import java.lang.foreign.ValueLayout.JAVA_INT
+import java.lang.foreign.MemorySegment
+import java.lang.foreign.SymbolLookup
+import java.lang.foreign.ValueLayout.*
 
 
 fun main() {
@@ -35,4 +36,34 @@ fun main() {
         Platform.load(Platform.libProvider(it))
     }*/
     Platform.init()
+    linker.downcallHandle(SymbolLookup.loaderLookup().find("glfwInit").get(), FunctionDescriptor.ofVoid()).invoke()
+    val window = linker.downcallHandle(
+        SymbolLookup.loaderLookup().find("glfwCreateWindow").get(),
+        FunctionDescriptor.of(ADDRESS, JAVA_INT, JAVA_INT, ADDRESS, JAVA_LONG, JAVA_LONG)
+    ).invoke(640, 480, offHeap.allocateUtf8String("test!"), 0L, 0L) as MemorySegment
+    linker.downcallHandle(
+        SymbolLookup.loaderLookup().find("glfwShowWindow").get(),
+        FunctionDescriptor.ofVoid(ADDRESS)
+    ).invoke(window)
+
+    while (!(linker.downcallHandle(
+            SymbolLookup.loaderLookup().find("glfwWindowShouldClose").get(),
+            FunctionDescriptor.of(JAVA_BOOLEAN, ADDRESS)
+        ).invoke(window) as Boolean)
+    ) {
+        linker.downcallHandle(
+            SymbolLookup.loaderLookup().find("glfwSwapBuffers").get(),
+            FunctionDescriptor.ofVoid(ADDRESS)
+        ).invoke(window)
+
+        linker.downcallHandle(
+            SymbolLookup.loaderLookup().find("glfwPollEvents").get(),
+            FunctionDescriptor.ofVoid()
+        ).invoke()
+    }
+
+    linker.downcallHandle(
+        SymbolLookup.loaderLookup().find("glfwDestroyWindow").get(),
+        FunctionDescriptor.ofVoid(ADDRESS)
+    ).invoke(window)
 }
