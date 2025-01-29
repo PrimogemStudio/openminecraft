098641ab8f08012157cbfb300ea3ee6207cad382
Jack253-png <guoxiuchen20170402@163.com>
2025-01-29 19:22:19 +0800

    更新 workflow, graalvm 兼容更改

--- nativeimage/src/main/kotlin/com/primogemstudio/engine/NativeImageFeature.kt
+++ nativeimage/src/main/kotlin/com/primogemstudio/engine/NativeImageFeature.kt
@@ -4,32 +4,71 @@ import org.graalvm.nativeimage.hosted.Feature
 import org.graalvm.nativeimage.hosted.Feature.DuringSetupAccess
 import java.lang.foreign.*
 import java.lang.foreign.ValueLayout.*
+import java.lang.foreign.MemoryLayout
 import java.lang.Class
 
 class NativeImageFeature: Feature {
-    override fun duringSetup(access: DuringSetupAccess) {
+	override fun duringSetup(access: DuringSetupAccess) {
 		val rfaClass = Class.forName("org.graalvm.nativeimage.hosted.RuntimeForeignAccess")
-		listOf(
-			FunctionDescriptor.of(JAVA_BOOLEAN),
-			FunctionDescriptor.of(ADDRESS, ADDRESS),
-			FunctionDescriptor.of(ADDRESS, ADDRESS, ADDRESS),
-			FunctionDescriptor.of(ADDRESS, JAVA_INT, JAVA_INT),
-			FunctionDescriptor.of(ADDRESS, JAVA_INT, JAVA_INT, ADDRESS, ADDRESS, ADDRESS),
-			FunctionDescriptor.of(JAVA_INT, ADDRESS, JAVA_INT, ADDRESS),
-			FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, ADDRESS),
-			FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, ADDRESS, ADDRESS),
-			FunctionDescriptor.of(ADDRESS, ADDRESS, JAVA_INT, JAVA_INT),
-			FunctionDescriptor.of(ADDRESS, ADDRESS, ADDRESS),
-			FunctionDescriptor.of(ADDRESS, JAVA_INT),
-			FunctionDescriptor.of(JAVA_INT, ADDRESS),
-			FunctionDescriptor.of(ADDRESS)
-		).forEach {
-			rfaClass.getMethod("registerForDowncall", Any::class.java, Array<Any>::class.java).invoke(null, it, arrayOf<Any>())
+		val regDowncall = rfaClass.getMethod("registerForDowncall", Any::class.java, Array<Any>::class.java)
+		val regUpcall = rfaClass.getMethod("registerForUpcall", Any::class.java, Array<Any>::class.java)
+
+		val basicTypes = listOf(JAVA_BOOLEAN, JAVA_LONG, JAVA_INT, JAVA_FLOAT, JAVA_DOUBLE, JAVA_BYTE, JAVA_SHORT, ADDRESS)
+		val downcallTypes = mutableListOf<FunctionDescriptor>()
+		val upcallTypes = mutableListOf<FunctionDescriptor>()
+		for (ret in basicTypes) {
+			downcallTypes.add(FunctionDescriptor.of(ret))
+			for (arg1 in basicTypes) {
+				downcallTypes.add(FunctionDescriptor.of(ret, arg1))
+				for (arg2 in basicTypes) {
+					downcallTypes.add(FunctionDescriptor.of(ret, arg1, arg2))
+					for (arg3 in basicTypes) {
+						downcallTypes.add(FunctionDescriptor.of(ret, arg1, arg2, arg3))
+						for (arg4 in basicTypes) {
+							downcallTypes.add(FunctionDescriptor.of(ret, arg1, arg2, arg3, arg4))
+							for (arg5 in basicTypes) {
+								downcallTypes.add(FunctionDescriptor.of(ret, arg1, arg2, arg3, arg4, arg5))
+							}
+						}
+					}
+				}
+			}
 		}
 
-		listOf(
-            FunctionDescriptor.ofVoid(JAVA_INT, ADDRESS),
-			FunctionDescriptor.ofVoid(ADDRESS, JAVA_INT, JAVA_INT)
-		).forEach { rfaClass.getMethod("registerForUpcall", Any::class.java, Array<Any>::class.java).invoke(null, it, arrayOf<Any>()) }
-    }
+		upcallTypes.add(FunctionDescriptor.ofVoid())
+		for (arg1 in basicTypes) {
+			upcallTypes.add(FunctionDescriptor.ofVoid(arg1))
+			for (arg2 in basicTypes) {
+				upcallTypes.add(FunctionDescriptor.ofVoid(arg1, arg2))
+				for (arg3 in basicTypes) {
+					upcallTypes.add(FunctionDescriptor.ofVoid(arg1, arg2, arg3))
+					for (arg4 in basicTypes) {
+						upcallTypes.add(FunctionDescriptor.ofVoid(arg1, arg2, arg3, arg4))
+						for (arg5 in basicTypes) {
+							upcallTypes.add(FunctionDescriptor.ofVoid(arg1, arg2, arg3, arg4, arg5))
+							for (arg6 in basicTypes) {
+								for (arg7 in basicTypes) {
+									upcallTypes.add(FunctionDescriptor.ofVoid(arg1, arg2, arg3, arg4, arg5, arg6, arg7))
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		// For harfbuzz paint api
+		upcallTypes.add(FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, JAVA_FLOAT, JAVA_FLOAT, JAVA_FLOAT, JAVA_FLOAT, JAVA_FLOAT, JAVA_FLOAT, ADDRESS))
+		upcallTypes.add(FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, JAVA_FLOAT, JAVA_FLOAT, JAVA_FLOAT, JAVA_FLOAT, ADDRESS))
+		upcallTypes.add(FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, ADDRESS, JAVA_INT, JAVA_INT, JAVA_INT, JAVA_FLOAT, ADDRESS, ADDRESS))
+		upcallTypes.add(FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, ADDRESS, JAVA_FLOAT, JAVA_FLOAT, JAVA_FLOAT, JAVA_FLOAT, JAVA_FLOAT, JAVA_FLOAT, ADDRESS))
+		upcallTypes.add(FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, ADDRESS, JAVA_FLOAT, JAVA_FLOAT, JAVA_FLOAT, JAVA_FLOAT, ADDRESS))
+
+		downcallTypes.forEach {
+			regDowncall.invoke(null, it, arrayOf<Any>())
+		}
+
+		upcallTypes.forEach {
+			regUpcall.invoke(null, it, arrayOf<Any>())
+		}
+	}
 }
