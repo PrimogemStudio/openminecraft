e7c61565008dfb5e1eacb66f2e4c5bb91a5379e5
Jack253-png <guoxiuchen20170402@163.com>
2025-02-02 19:32:44 +0800

    vk 函数包装

--- nativeloader/src/main/kotlin/com/primogemstudio/engine/interfaces/heap/IHeapVar.kt
+++ nativeloader/src/main/kotlin/com/primogemstudio/engine/interfaces/heap/IHeapVar.kt
@@ -1,9 +1,8 @@
 package com.primogemstudio.engine.interfaces.heap
 
 import java.lang.foreign.MemorySegment
-import java.io.Closeable
 
-interface IHeapVar<T>: Closeable {
+interface IHeapVar<T> {
     fun ref(): MemorySegment
     fun value(): T
 }
