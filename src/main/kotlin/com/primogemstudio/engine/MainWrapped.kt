13c28c3b3aa526ab88ec48a3838f26deafe91024
Jack253-png <guoxiuchen20170402@163.com>
2025-02-06 19:22:15 +0800

    vk 结构体重写

--- src/main/kotlin/com/primogemstudio/engine/MainWrapped.kt
+++ src/main/kotlin/com/primogemstudio/engine/MainWrapped.kt
@@ -105,7 +105,6 @@ fun main() {
             }, { logger.error("vulkan error: $it") })
         }, { logger.error("vulkan error: $it") })
     }, { logger.error("vulkan error: $it") })
-    println(Class.forName("org.lwjgl.vulkan.VkPipelineRasterizationStateCreateInfo").getField("SIZEOF").get(null))
 
     glfwSetCursor(
         window,
