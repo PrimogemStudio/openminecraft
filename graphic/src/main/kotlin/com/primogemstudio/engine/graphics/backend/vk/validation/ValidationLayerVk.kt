b83d7afbd79b0016447dd2245531e846fa88b855
Jack253-png <guoxiuchen20170402@163.com>
2025-02-22 13:46:27 +0800

    vulkan 验证层封装

--- graphic/src/main/kotlin/com/primogemstudio/engine/graphics/backend/vk/validation/ValidationLayerVk.kt
+++ graphic/src/main/kotlin/com/primogemstudio/engine/graphics/backend/vk/validation/ValidationLayerVk.kt
@@ -1,5 +1,6 @@
 package com.primogemstudio.engine.graphics.backend.vk.validation
 
+import com.primogemstudio.engine.bindings.vulkan.ext.debugutils.VkDebugUtilsMessengerCallbackDataEXT
 import com.primogemstudio.engine.bindings.vulkan.ext.debugutils.VkDebugUtilsMessengerCallbackEXT
 import com.primogemstudio.engine.bindings.vulkan.ext.debugutils.VkDebugUtilsMessengerCreateInfoEXT
 import com.primogemstudio.engine.bindings.vulkan.ext.debugutils.VkEXTDebugUtils
@@ -15,6 +16,7 @@ import com.primogemstudio.engine.bindings.vulkan.vk10.Vk10Funcs.VK_SUCCESS
 import com.primogemstudio.engine.bindings.vulkan.vk10.Vk10Funcs.vkEnumerateInstanceExtensionProperties
 import com.primogemstudio.engine.bindings.vulkan.vk10.Vk10Funcs.vkEnumerateInstanceLayerProperties
 import com.primogemstudio.engine.bindings.vulkan.vk10.VkInstance
+import com.primogemstudio.engine.i18n.Internationalization.tr
 import com.primogemstudio.engine.logging.LoggerFactory
 import java.io.Closeable
 
@@ -33,6 +35,25 @@ class ValidationLayerVk : Closeable {
         .apply { if (hasValidationLayer()) add(VkEXTDebugUtils.VK_EXT_DEBUG_UTILS_EXTENSION_NAME) }
         .toTypedArray()
 
+    private fun logActual(severity: Int, type: Int, data: VkDebugUtilsMessengerCallbackDataEXT) {
+        val type = tr(
+            when (type) {
+                VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT -> "engine.renderer.backend_vk.type.general"
+                VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT -> "engine.renderer.backend_vk.type.validation"
+                VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT -> "engine.renderer.backend_vk.type.performance"
+                else -> "engine.renderer.backend_vk.type.general"
+            }
+        )
+
+        when (severity) {
+            VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT -> logger.debug("[$type] ${data.message}")
+            VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT -> logger.info("[$type] ${data.message}")
+            VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT -> logger.warn("[$type] ${data.message}")
+            VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT -> logger.error("[$type] ${data.message}")
+            else -> logger.info("[$type] ${data.message}")
+        }
+    }
+
     fun instanceAttach(instance: VkInstance) {
         vkCreateDebugUtilsMessengerEXT(instance, VkDebugUtilsMessengerCreateInfoEXT().apply {
             messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT or
@@ -42,8 +63,8 @@ class ValidationLayerVk : Closeable {
             messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT or
                     VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT or
                     VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT
-            callback = VkDebugUtilsMessengerCallbackEXT { severity, type, callbackData, user ->
-                logger.info("$severity $type ${callbackData.message}")
+            callback = VkDebugUtilsMessengerCallbackEXT { severity, type, callbackData, _ ->
+                logActual(severity, type, callbackData)
                 VK_SUCCESS
             }
         }, null)
