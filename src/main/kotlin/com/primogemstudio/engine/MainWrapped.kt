4af3df2361d4ca671522048d8d1b77bd1826238c
Coder2 <83895170+Jack253-png@users.noreply.github.com>
2025-03-10 19:37:40 +0800

    Update MainWrapped.kt

--- src/main/kotlin/com/primogemstudio/engine/MainWrapped.kt
+++ src/main/kotlin/com/primogemstudio/engine/MainWrapped.kt
@@ -13,6 +13,7 @@ import java.io.ByteArrayInputStream
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import kotlin.concurrent.thread
+import kotlin.random.Random
 
 suspend fun main() {
     /*val t = JsonLexer(ByteArrayInputStream("{\"test\": 114514, \"test2\": 1144.11, \"test3\": \"test\"}".toByteArray()))
