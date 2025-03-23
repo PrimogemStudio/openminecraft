b80a0f61a7497075f077ef23e94bc588c55f1aa9
Jack253-png <guoxiuchen20170402@163.com>
2025-03-23 19:05:30 +0800

    harfbuzz binding

--- binding/src/main/kotlin/com/primogemstudio/engine/bindings/harfbuzz/HarfBuzzFuncs.kt
+++ binding/src/main/kotlin/com/primogemstudio/engine/bindings/harfbuzz/HarfBuzzFuncs.kt
@@ -1,6 +1,11 @@
 package com.primogemstudio.engine.bindings.harfbuzz
 
+import com.primogemstudio.engine.foreign.NativeMethodCache.callPointerFunc
+import com.primogemstudio.engine.foreign.NativeMethodCache.callVoidFunc
+import com.primogemstudio.engine.foreign.NativeMethodCache.constructStub
+import com.primogemstudio.engine.foreign.heap.HeapByteArray
 import com.primogemstudio.engine.foreign.heap.IHeapObject
+import com.primogemstudio.engine.foreign.toCString
 import java.lang.foreign.MemorySegment
 
 class hb_blob_t(data: MemorySegment) : IHeapObject(data)
@@ -10,5 +15,50 @@ object HarfBuzzFuncs {
     const val HB_MEMORY_MODE_READONLY = 1
     const val HB_MEMORY_MODE_WRITABLE = 2
     const val HB_MEMORY_MODE_READONLY_MAY_MAKE_WRITABLE = 3
+
+    fun hb_blob_create(data: HeapByteArray, mode: Int, userData: MemorySegment, destroy: hb_destroy_func_t): hb_blob_t =
+        hb_blob_t(
+            callPointerFunc(
+                "hb_blob_create",
+                data,
+                data.length,
+                mode,
+                userData,
+                constructStub(hb_destroy_func_t::class, destroy)
+            )
+        )
+
+    fun hb_blob_create_or_fail(
+        data: HeapByteArray,
+        mode: Int,
+        userData: MemorySegment,
+        destroy: hb_destroy_func_t
+    ): hb_blob_t =
+        hb_blob_t(
+            callPointerFunc(
+                "hb_blob_create_or_fail",
+                data,
+                data.length,
+                mode,
+                userData,
+                constructStub(hb_destroy_func_t::class, destroy)
+            )
+        )
+
+    fun hb_blob_create_from_file(fileName: String): hb_blob_t =
+        hb_blob_t(callPointerFunc("hb_blob_create_from_file", fileName.toCString()))
+
+    fun hb_blob_create_from_file_or_fail(fileName: String): hb_blob_t =
+        hb_blob_t(callPointerFunc("hb_blob_create_from_file_or_fail", fileName.toCString()))
+
+    fun hb_blob_create_sub_blob(blob: hb_blob_t, offset: Int, length: Int): hb_blob_t =
+        hb_blob_t(callPointerFunc("hb_blob_create_sub_blob", blob, offset, length))
+
+    fun hb_blob_copy_writable_or_fail(data: hb_blob_t): hb_blob_t =
+        hb_blob_t(callPointerFunc("hb_blob_copy_writable_or_fail", data))
+
+    fun hb_blob_get_empty(): hb_blob_t = hb_blob_t(callPointerFunc("hb_blob_get_empty"))
+    fun hb_blob_reference(blob: hb_blob_t): hb_blob_t = hb_blob_t(callPointerFunc("hb_blob_reference", blob))
+    fun hb_blob_destroy(blob: hb_blob_t) = callVoidFunc("hb_blob_destroy", blob)
 
 }
