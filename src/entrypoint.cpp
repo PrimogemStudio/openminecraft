a43dc339c8f33036b7bcc0ef7e7ac42ed6002316
Coder2 <83895170+Jack253-png@users.noreply.github.com>
2025-03-31 19:45:27 +0800

    Update entrypoint.cpp

--- src/entrypoint.cpp
+++ src/entrypoint.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
-#include <vulkan/vulkan_core.h>
+#define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
+#include "vulkan/vulkan.hpp"
 
-int main() {
-    uint32_t d;
-    vkEnumerateInstanceLayerProperties(&d, nullptr);
-    std::cout << "test!" << d << std::endl;
-    return 0;
+// Vulkan defined macro storage for dispatch loader
+VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
+
+int main()
+{
+    vk::DynamicLoader dl;
+    PFN_vkGetInstanceProcAddr vkGetInstanceProcAddr = dl.getProcAddress<PFN_vkGetInstanceProcAddr>("vkGetInstanceProcAddr");
+    VULKAN_HPP_DEFAULT_DISPATCHER.init(vkGetInstanceProcAddr);
+    vk::Instance instance = vk::createInstance({}, nullptr);
+    // initialize function pointers for instance
+    VULKAN_HPP_DEFAULT_DISPATCHER.init(instance);
+    // create a dispatcher, based on additional vkDevice/vkGetDeviceProcAddr
+    std::vector<vk::PhysicalDevice> physicalDevices = instance.enumeratePhysicalDevices();  
+    vk::Device device = physicalDevices[0].createDevice({}, nullptr);
+    // function pointer specialization for device
+    VULKAN_HPP_DEFAULT_DISPATCHER.init(device);
 }
