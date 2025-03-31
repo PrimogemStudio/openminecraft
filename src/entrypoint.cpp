3facd01f01d9f39b841e8edd63a751c516cb7365
Coder2 <83895170+Jack253-png@users.noreply.github.com>
2025-03-31 20:52:30 +0800

    Update entrypoint.cpp

--- src/entrypoint.cpp
+++ src/entrypoint.cpp
@@ -1,5 +1,6 @@
 #define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
 #include "vulkan/vulkan.hpp"
+#include <iostream>
 
 // Vulkan defined macro storage for dispatch loader
 VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
@@ -14,6 +15,7 @@ int main()
     VULKAN_HPP_DEFAULT_DISPATCHER.init(instance);
     // create a dispatcher, based on additional vkDevice/vkGetDeviceProcAddr
     std::vector<vk::PhysicalDevice> physicalDevices = instance.enumeratePhysicalDevices();  
+    std::cout << physicalDevices.size();
     vk::Device device = physicalDevices[0].createDevice({}, nullptr);
     // function pointer specialization for device
     VULKAN_HPP_DEFAULT_DISPATCHER.init(device);
