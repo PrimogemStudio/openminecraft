25193e779b05bb70e4aebca058765eb9718058cb
Coder2 <83895170+Jack253-png@users.noreply.github.com>
2025-04-03 22:28:37 +0800

    Update glfw.lua

--- extlibs/glfw.lua
+++ extlibs/glfw.lua
@@ -1,3 +1,4 @@
+if not is_arch("loong64") then
 package("libxau")
     set_homepage("https://www.x.org/")
     set_description("X.Org: A Sample Authorization Protocol for X")
@@ -8,6 +9,7 @@ package("libxau")
         import("package.tools.xmake").install(package, configs)
     end)
 package_end()
+end
 
 if is_arch("loong64") then
     package("libxdmcp")
