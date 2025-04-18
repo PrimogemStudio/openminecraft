d2485e704a60dff78d0458d970d10427f0aee9d0
Coder2 <83895170+Jack253-png@users.noreply.github.com>
2025-04-19 05:28:52 +0800

    Update entrypoint.cpp

--- src/entrypoint.cpp
+++ src/entrypoint.cpp
@@ -60,12 +60,12 @@ int main()
         // SDL_GetError());
     }
 
-    /*if (!SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Hello World",
+    if (!SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Hello World",
                                  "!! Your SDL project successfully runs on Android
     !!", NULL)) {
-        // SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_ShowSimpleMessageBox
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_ShowSimpleMessageBox
     failed (%s)", SDL_GetError()); return 1;
-    }*/
+    }
 
     SDL_Quit();
 
