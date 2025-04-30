27d38bdd76b5f64a8ddd1218b046dccabf8c1a27
Jack253-png <guoxiuchen20170402@163.com>
2025-04-30 22:29:51 +0800

    json tokens

--- include/openminecraft/parser/om_parser_json.hpp
+++ include/openminecraft/parser/om_parser_json.hpp
@@ -5,15 +5,37 @@
 #include "openminecraft/log/om_log_common.hpp"
 #include <istream>
 #include <memory>
+#include <vector>
 
 namespace openminecraft::parser::json
 {
+enum OMJsonTokenType
+{
+    JsonEmpty,
+    JsonNull,
+    JsonBool,
+    JsonObjectStart,
+    JsonObjectEnd,
+    JsonArrayStart,
+    JsonArrayEnd,
+    JsonStringLitrStart,
+    JsonStringLitrEnd,
+    JsonComma,
+    JsonColon,
+    JsonNumber,
+    JsonString
+};
+struct OMJsonToken
+{
+    OMJsonTokenType type;
+    std::string token;
+};
 class OMParserJson : io::OMParser
 {
   public:
     OMParserJson(std::shared_ptr<std::istream> s);
     ~OMParserJson();
-    void parseMap();
+    std::vector<OMJsonToken> parse();
 
   private:
     std::shared_ptr<log::OMLogger> logger;
--- parser/om_parser_json.cpp
+++ parser/om_parser_json.cpp
@@ -2,8 +2,10 @@
 #include "openminecraft/io/om_io_parser.hpp"
 #include "openminecraft/log/om_log_common.hpp"
 #include "openminecraft/util/om_util_string.hpp"
+#include <cstdio>
 #include <istream>
 #include <memory>
+#include <vector>
 
 using namespace openminecraft::util::string;
 namespace openminecraft::parser::json
@@ -17,9 +19,10 @@ OMParserJson::~OMParserJson()
     io::OMParser::~OMParser();
 }
 
-void OMParserJson::parseMap()
+std::vector<OMJsonToken> OMParserJson::parse()
 {
     bool inString = false;
+    std::vector<OMJsonToken> data;
     while (source->good())
     {
         int k = utf8Next(source);
@@ -27,32 +30,32 @@ void OMParserJson::parseMap()
         switch (k)
         {
         case '{': {
-            logger->info("JsonObjectStart");
+            data.push_back({JsonObjectStart, "{"});
             break;
         }
         case '}': {
-            logger->info("JsonObjectEnd");
+            data.push_back({JsonObjectEnd, "}"});
             break;
         }
         case '[': {
-            logger->info("JsonArrayStart");
+            data.push_back({JsonArrayStart, "["});
             break;
         }
         case ']': {
-            logger->info("JsonArrayEnd");
+            data.push_back({JsonArrayEnd, "]"});
             break;
         }
         case '"': {
-            logger->info("JsonStringLitr{}", inString ? "End" : "Start");
+            data.push_back({(inString ? JsonStringLitrEnd : JsonStringLitrStart), "\""});
             inString = !inString;
             break;
         }
         case ':': {
-            logger->info("JsonColon");
+            data.push_back({JsonColon, ":"});
             break;
         }
         case ',': {
-            logger->info("JsonComma");
+            data.push_back({JsonComma, ","});
             break;
         }
         case '.':
@@ -66,16 +69,85 @@ void OMParserJson::parseMap()
         case '7':
         case '8':
         case '9': {
-            logger->info("JsonNumber");
+            std::vector<int> d;
+            d.push_back(k);
+            data.push_back({JsonNumber, uniToString(d)});
             break;
         }
         default: {
+#define CheckChar(c) source->peek() == c
+#define For(o) for (int i = 0; i < o; i++)
             if (inString)
             {
-                logger->info("JsonString");
+                std::vector<int> d;
+                d.push_back(k);
+                data.push_back({JsonString, uniToString(d)});
+            }
+            else if (k == 't' && CheckChar('r') && CheckChar('u') && CheckChar('e'))
+            {
+                data.push_back({JsonBool, "true"});
+                For(3) k += source->get();
+            }
+            else if (k == 'f' && CheckChar('a') && CheckChar('l') && CheckChar('s') && CheckChar('e'))
+            {
+                data.push_back({JsonBool, "false"});
+                For(4) k += source->get();
+            }
+            else if (k == 'n' && CheckChar('u') && CheckChar('l') && CheckChar('l'))
+            {
+                data.push_back({JsonBool, "null"});
+                For(3) k += source->get();
             }
         }
         }
     }
+
+    std::vector<OMJsonToken> merged;
+    for (int i = 0; i < data.size(); i++)
+    {
+        if (i == data.size() - 1)
+        {
+            merged.push_back(data[i]);
+        }
+        else if (data[i].type == JsonNumber)
+        {
+            std::string r = "";
+            for (int j = i; j < data.size(); j++)
+            {
+                if (data[j].type != JsonNumber)
+                {
+                    i = j - 1;
+                    break;
+                }
+                r += (data[j].token);
+            }
+            merged.push_back({JsonNumber, r});
+        }
+        else if (data[i].type == JsonString)
+        {
+            std::string r = "";
+            for (int j = i; j < data.size(); j++)
+            {
+                if (data[j].type != JsonString)
+                {
+                    i = j - 1;
+                    break;
+                }
+                r += (data[j].token);
+            }
+            merged.push_back({JsonString, r});
+        }
+        else
+        {
+            merged.push_back(data[i]);
+        }
+    }
+
+    for (auto a : merged)
+    {
+        logger->info("{}", a.token);
+    }
+
+    return data;
 }
 } // namespace openminecraft::parser::json
