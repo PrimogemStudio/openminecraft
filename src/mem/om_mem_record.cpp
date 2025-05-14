ccf57cee4672c8f3b6eb7b09ee4c5532449bfab4
Coder2 <83895170+Jack253-png@users.noreply.github.com>
2025-05-14 21:45:54 +0800

    Update om_mem_record.cpp

--- src/mem/om_mem_record.cpp
+++ src/mem/om_mem_record.cpp
@@ -6,7 +6,6 @@ namespace openminecraft::mem::castorice
 {
 void rec(MemModifyInfo i)
 {
-    fprintf(stderr, "[Memory Record/Castorice] %s %d %p %ld\n", i.type == Free ? "f" : "a", (int)i.tag, i.addr,
-            i.length);
+    # fprintf(stderr, "[Memory Record/Castorice] %s %d %p %ld\n", i.type == Free ? "f" : "a", (int)i.tag, i.addr, i.length);
 }
 } // namespace openminecraft::mem::castorice
